0555281b64dafc5630fd684edcea3461c37894ce
Victor <vlam.pard.1403k@gmail.com>
2015-06-05 22:09:42 -0500

    Desarrolo de la pantalla registro de Empresa

--- Votacion_WebSite/RegistrarEmpresas.aspx.cs
+++ Votacion_WebSite/RegistrarEmpresas.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Votacion_BO;
+using System.Web.Configuration;
+using System.Globalization;
+using System.Data;
 
 namespace Votacion_WebSite
 {
@@ -12,6 +16,31 @@ namespace Votacion_WebSite
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnRegistrarEmpresa_Click(object sender, EventArgs e)
+        {
+            VoUser user = new VoUser();
+            user.Nombres = txtNombreAdm.Text;
+            user.Apellidos = txtApellidoAdm.Text;
+            user.TypeDocument = txtTipoDocAdm.Text;
+            user.Identficacion = txtNumDocAdm.Text;
+            user.Numero = 0;
+            user.IdRol = 2;
+            user.IdTipoUsuario = 3;
+            user.Genero = ddlGeneroAdm.SelectedValue;
+            user.NombreUsuario = txtNickUser.Text;
+            user.Contrasenia = txtPass.Text;
+            user.FechaNacimiento = DateTime.Parse(txtFechaNacimientoAdmin.Text);
+
+            VoCompany empresa = new VoCompany();
+            empresa.NitEmpresa = txtNitEmpresa.Text;
+            empresa.NombreEmpresa = txtNombreEmpresa.Text;
+            empresa.TelefonoEmpresa = txtTelefonoEmpresa.Text;
+            empresa.DireccionEmpresa = txtDireccionEmpresa.Text;
+            empresa.CorreoEmpresa = txtMailEmpresa.Text;
+            new LogicCompany().RegistroNuevaEmpresaAdmin(empresa, user);
+            Response.Redirect("/Inicio.aspx");
         }
     }
 }
